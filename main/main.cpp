a2f96e7f58030fd27df6de0b24227befddf79514
PavelKitaev <A359@live.ru>
2020-11-14 17:38:09 +0300

    travis

--- main/main.cpp
+++ main/main.cpp
@@ -4,17 +4,17 @@
 int main()
 {
   TArrayList<char> temp(10);
-  temp.InsFirst('a');
-  temp.InsLast('b');
-  temp.InsFirst('c');
+  //temp.InsFirst('a');
+  //temp.InsLast('b');
+  //temp.InsFirst('c');
 
-  TArrayListIterator<char> i = temp.Begin();
-  while (i.IsGoNext())
-  {
-    cout << i.GetData();
-    i.GoNext();
-  }
+  //TArrayListIterator<char> i = temp.Begin();
+  //while (i.IsGoNext())
+  //{
+  //  cout << i.GetData();
+  //  i.GoNext();
+  //}
 
-  cout << temp << "\n";
+  //cout << temp << "\n";
   return 0;
 }
